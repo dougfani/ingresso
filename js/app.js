604a4dcffe732d3371c9505b3c54df80068ffec1
Douglas <douglas97floriano@gmail.com>
2025-08-03 17:27:37 -0300

    refatorar lógica de compra de ingressos, movendo validações para funções específicas. Mudanças feitas de acordo com sugestão dos tutores do curso da alura

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
-// recuperar quantidade de ingresoss disponíveis
-    let qtdTotalInferior = document.getElementById('qtd-inferior');
-    let qtdTotalInferiorNumero = parseInt(qtdTotalInferior.textContent);
-    let qtdTotalSuperior = document.getElementById('qtd-superior');
-    let qtdTotalSuperiorNumero = parseInt(qtdTotalSuperior.textContent);
-    let qtdTotalPista = document.getElementById('qtd-pista');
-    let qtdTotalPistaNumero = parseInt(qtdTotalPista.textContent);
-
 function comprar() {
-    // recuperar dados do ingresso
     tipoIngresso = document.getElementById('tipo-ingresso').value;
     quantidade = parseInt(document.getElementById('qtd').value);
 
@@ -19,38 +10,47 @@ function comprar() {
     }
 
     if (tipoIngresso == 'inferior') {
-        if (qtdTotalInferiorNumero == 0) {
-            alert('Ingressos para Cadeira Inferior esgotados!');
-            return;
-        } else if (qtdTotalInferiorNumero < quantidade) {
-            alert('Quantidade solicitada indisponível!');
-            return;
-        } else {
-            qtdTotalInferiorNumero = qtdTotalInferiorNumero - quantidade;
-            qtdTotalInferior.textContent = qtdTotalInferiorNumero;
-        }
+        comprarInferior(quantidade);
     } else if (tipoIngresso == 'superior') {
-        if (qtdTotalSuperiorNumero == 0) {
-            alert('Ingressos para Cadeira Superior esgotados!');
-            return;
-        } else if (qtdTotalSuperiorNumero < quantidade) {
-            alert('Quantidade solicitada indisponível!');
-            return;
-        } else {
-            qtdTotalSuperiorNumero = qtdTotalSuperiorNumero - quantidade;
-            qtdTotalSuperior.textContent = qtdTotalSuperiorNumero;
-        }
-    } else if (tipoIngresso == 'pista') {
-        if (qtdTotalPistaNumero == 0) {
-            alert('Ingressos para Pista esgotados!');
-            return;
-        } else if (qtdTotalPistaNumero < quantidade) {
-            alert('Quantidade solicitada indisponível!');
-            return;
-        } else {
-            qtdTotalPistaNumero = qtdTotalPistaNumero - quantidade;
-            qtdTotalPista.textContent = qtdTotalPistaNumero;
-        }
+        comprarSuperior(quantidade);
+    } else {
+        comprarPista(quantidade);
     }
     document.getElementById('qtd').value = 0;
+}
+
+function comprarInferior(quantidade) {
+    let qtdTotalInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (quantidade > qtdTotalInferior) {
+        alert('Quantidade indisponível para tipo cadeira inferior!');
+        return;
+    } else {
+        qtdTotalInferior = qtdTotalInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = qtdTotalInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(quantidade) {
+    let qtdTotalSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (quantidade > qtdTotalSuperior) {
+        alert('Quantidade indisponível para tipo cadeira superior!');
+        return;
+    } else {
+        qtdTotalSuperior = qtdTotalSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = qtdTotalSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarPista(quantidade) {
+    let qtdTotalPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (quantidade > qtdTotalPista) {
+        alert('Quantidade indisponível para tipo pista!');
+        return;
+    } else {
+        qtdTotalPista = qtdTotalPista - quantidade;
+        document.getElementById('qtd-pista').textContent = qtdTotalPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
